42607266e6eb78c9aae8dc2904ef6b1b580f74f0
Joseph Brock <joseph.brock@canonical.com>
2024-02-19 14:58:27 -0500

    ensure that XDG_RUNTIME_DIR exists

--- src/spotifyd.sh
+++ src/spotifyd.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+[ -d "$XDG_RUNTIME_DIR" ] || \
+    mkdir -p "$XDG_RUNTIME_DIR"
+
 "${SNAP}/bin/spotifyd" \
     --config-path "${SNAP_COMMON}/spotifyd.conf" \
     --cache-path "${SNAP_COMMON}/cache" \
